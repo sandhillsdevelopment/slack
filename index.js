31303166ef16dc412400959f71bf0b3dd584238f
DrewAPicture <info@drewapicture.com>
2017-07-23 01:07:27 -0600

    Revert to 12 hours ago.

--- index.js
+++ index.js
@@ -1,92 +1,89 @@
 #!/usr/bin/env node
 var Client  = require('slack-client'),
-    request = require('request');
-    fs      = require('fs');
+	request = require('request');
 
 module.exports = Client;
- 
+
 // Create a new bot at https://YOURSLACK.slack.com/services/new/bot
-var BOT_TOKEN = 'xoxb-215585930114-ndEzrGimPtXheUbyZ5pUgrHa',
-    AUTH_TOKEN = fs.readFileSync( 'secrets' );
+var BOT_TOKEN = 'xoxb-215585930114-ndEzrGimPtXheUbyZ5pUgrHa';
 
 var slack = new Client(BOT_TOKEN, true, true);
 
 slack.on('open', function () {
-    var channels = Object.keys(slack.channels)
-        .map(function (k) { return slack.channels[k]; })
-        .filter(function (c) { return c.is_member; })
-        .map(function (c) { return c.name; });
- 
-    var groups = Object.keys(slack.groups)
-        .map(function (k) { return slack.groups[k]; })
-        .filter(function (g) { return g.is_open && !g.is_archived; })
-        .map(function (g) { return g.name; });
- 
-    console.log('Welcome to Slack. You are ' + slack.self.name + ' of ' + slack.team.name);
- 
-    if (channels.length > 0) {
-        console.log('You are in: ' + channels.join(', '));
-    }
-    else {
-        console.log('You are not in any channels.');
-    }
- 
-    if (groups.length > 0) {
-       console.log('As well as: ' + groups.join(', '));
-    }
+	var channels = Object.keys(slack.channels)
+		.map(function (k) { return slack.channels[k]; })
+		.filter(function (c) { return c.is_member; })
+		.map(function (c) { return c.name; });
+
+	var groups = Object.keys(slack.groups)
+		.map(function (k) { return slack.groups[k]; })
+		.filter(function (g) { return g.is_open && !g.is_archived; })
+		.map(function (g) { return g.name; });
+
+	console.log('Welcome to Slack. You are ' + slack.self.name + ' of ' + slack.team.name);
+
+	if (channels.length > 0) {
+		console.log('You are in: ' + channels.join(', '));
+	}
+	else {
+		console.log('You are not in any channels.');
+	}
+
+	if (groups.length > 0) {
+		console.log('As well as: ' + groups.join(', '));
+	}
 });
- 
+
 // when someone posts to the channel
 slack.on('message', function(message) {
-    var	channel = slack.getChannelGroupOrDMByID(message.channel),
-		user = slack.getUserByID(message.user),
-		repo = getRepoFromChannel( channel.name );
+	var channel = slack.getChannelGroupOrDMByID(message.channel);
+	var user = slack.getUserByID(message.user);
+	var repo = getRepoFromChannel( channel.name );
 
-    // if we find a #...
-    if (message.type === 'message' && message.hasOwnProperty('text') && message.text.indexOf('#') > -1) {
-      var issueNum = message.text.substr(message.text.indexOf('#')).split(' ')[0];
-      var abbr     = issueNum.match( /[a-z]+/ );
+	// if we find a #...
+	if (message.type === 'message' && message.hasOwnProperty('text') && message.text.indexOf('#') > -1) {
+		var issueNum = message.text.substr(message.text.indexOf('#')).split(' ')[0];
+		var abbr     = issueNum.match( /[a-z]+/ );
 
-      console.log('Detected');
-      console.log(abbr);
+		console.log('Detected');
+		console.log(issueNum);
+		console.log(abbr);
+		if ( null !== abbr ) {
+			repo = getRepoFromAbbr( abbr[0], channel.name );
 
-      if ( null !== abbr ) {
-		  repo = getRepoFromAbbr( abbr[0], channel.name );
+			// Rewrite the issueNum minus the abbreviation.
+			issueNum = '#' + issueNum.match(/\d+$/);
+		}
 
-		  // Rewrite the issueNum minus the abbreviation.
-		  issueNum = '#' + issueNum.match(/\d+$/);
-      }
+		console.log(repo);
+		console.log(issueNum);
 
-      console.log(repo);
-      console.log(issueNum);
+		if (/^#\d+$/.test(issueNum)) {
+			var issueDescription,
+				options = {
+					url: 'https://api.github.com/repos/' + repo + '/issues/' + issueNum.substr(1),
+					method: 'GET',
+					headers: {
+						'User-Agent':   'Super Agent/0.0.1',
+						'Content-Type': 'application/x-www-form-urlencoded'
+					}
+				};
 
-      if (/^#\d+$/.test(issueNum)) {
-        var issueDescription,
-            options = {
-              url: 'https://api.github.com/repos/' + repo + '/issues/' + issueNum.substr(1),
-              method: 'GET',
-              headers: {
-                'User-Agent':   'Super Agent/0.0.1',
-                'Content-Type': 'application/x-www-form-urlencoded'
-              }
-            };
+			console.log( 'Repo URL:' );
+			console.log( options.url );
 
-        console.log( 'Repo URL:' );
-        console.log( options.url );
-
-        //Github API requires User Agent
-        request(options, function (error, response, body) {
-          var json = JSON.parse(body);
-          if (!error && response.statusCode == 200) {
-            issueDescription = "[#" + json.number + "] " + json.title + "\n " + json.html_url;
-
-            channel.send(issueDescription)
-          } else {
-			console.log( response );
-		  }
-        });
-      }
-    }
+			//Github API requires User Agent
+			request(options, function (error, response, body) {
+				var json = JSON.parse(body);
+				if (!error && response.statusCode == 200) {
+					issueDescription = "[#" + json.number + "] " + json.title + "\n " + json.html_url;
+					channel.send(issueDescription)
+				} else {
+					console.log( error );
+				}
+			});
+		}
+	}
 });
 
 function getRepoFromChannel( channel ) {
