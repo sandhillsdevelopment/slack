c00bd9dddd4f54ea252feee4e790ef1ddc84baa1
DrewAPicture <info@drewapicture.com>
2017-07-23 00:47:27 -0600

    Label the response in the log.

--- index.js
+++ index.js
@@ -84,6 +84,7 @@ slack.on('message', function(message) {
           if (!error && response.statusCode == 200) {
             issueDescription = "[#" + json.number + "] " + json.title + "\n " + json.html_url;
 
+            console.log( 'Response' );
             console.log( issueDescription );
             channel.send(issueDescription)
           } else {
