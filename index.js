3fabd1c81a35ef98c93c43768203c036e52a3713
DrewAPicture <info@drewapicture.com>
2017-07-25 23:36:51 -0600

    Initialize toPrint as an empty string instead of null to try to avoid undefined output.

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ slack.on('message', function(message) {
 
 		} else if ( message.text.match( /^\<@U6BH7TC3C\>\slist$/ ) ) {
 
-			var mapObject, toPrint;
+			var mapObject, toPrint = '';
 
 			switch( channel.name ) {
 				case 'affwp-general':
